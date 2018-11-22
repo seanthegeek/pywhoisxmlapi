1958e2de15f6b985cf4972205e19f79b295a9aea
Sean Whalen <whalenster@gmail.com>
2018-11-21 19:37:06 -0500

    PEP8 fixes

--- pywhoisxmlapi.py
+++ pywhoisxmlapi.py
@@ -3,10 +3,6 @@
 """An unofficial client for WhoisXMLAPI"""
 
 import os
-import json
-import base64
-import hashlib
-import hmac
 import time
 import datetime
 import logging
@@ -29,7 +25,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
 logging.basicConfig(
         format='%(asctime)s [%(levelname)s] %(message)s'
@@ -48,7 +44,8 @@ def epoch_to_datetime(epoch):
 
     """
     epoch_seconds = int(epoch) / 1000.0
-    return datetime.datetime.fromtimestamp(epoch_seconds).strftime('%Y-%m-%d %H:%M:%S.%f')
+    return datetime.datetime.fromtimestamp(epoch_seconds).strftime(
+        '%Y-%m-%d %H:%M:%S.%f')
 
 
 class WhoisXMLAPIError(RuntimeError):
@@ -69,26 +66,23 @@ class WhoisXMLAPI(object):
         Configures the API client
         
         Args:
-            api_key (str): WhoisXMLAPI API key; overridden by the ``WHOIS_KEY`` environment variable
-            
-        Warning:
-            Currently, only a few WHOISXMLAPI actions support API keys.
-            For now, you must use username/password authentication instead to use the full API.
+            api_key (str): WhoisXMLAPI API key; overridden by the
+            ``WHOIS_KEY`` environment variable
         """
+
         if "WHOIS_KEY" in os.environ:
             api_key = os.environ["WHOIS_KEY"]
-        if "WHOIS_EMAIL_KEY" in os.environ:
-            email_key = os.environ["WHOIS_EMAIL_KEY"]
-
         if "WHOIS_KEY" in os.environ:
             api_key = os.environ["WHOIS_KEY"]
         if api_key is None:
             raise ValueError(
-                "API key must provided as the api_key parameter, or the WHOIS_API_KEY environment variable")
+                "API key must provided as the api_key parameter, or the "
+                "WHOIS_KEY environment variable")
 
         self._api_key = api_key
         self._session = session()
-        self._session.headers.update({"User-Agent": "pywhoisxmlapi/{0}".format(__version__)})
+        self._session.headers.update(
+            {"User-Agent": "pywhoisxmlapi/{0}".format(__version__)})
 
     def _request(self, endpoint, params=None, post=False, parse_json=True):
         """
@@ -122,12 +116,15 @@ class WhoisXMLAPI(object):
         redacted_url = response.url
         if "callback" in params.keys() and params["callback"]:
             parse_json = False
-            redacted_url = redacted_url.replace(quote(self._api_key.encode("utf-8")), "REDACTED")
+            redacted_url = redacted_url.replace(
+                quote(self._api_key.encode("utf-8")), "REDACTED")
         if parse_json:
             response = response.json()
             if "ErrorMessage" in response:
                 error = response["ErrorMessage"]
-                raise WhoisXMLAPIError("{0}\nAttempted query: {1}".format(error["msg"], redacted_url))
+                raise WhoisXMLAPIError(
+                    "{0}\nAttempted query: {1}".format(error["msg"],
+                                                       redacted_url))
         else:
             response = response.text
 
@@ -146,12 +143,15 @@ class WhoisXMLAPI(object):
 
         return self._request(endpoint, params)
 
-    def set_account_balance_warnings(self, warn_threshold=10, warn_threshold_enabled=True, warn_empty_enabled=True):
+    def set_account_balance_warnings(self, warn_threshold=10,
+                                     warn_threshold_enabled=True,
+                                     warn_empty_enabled=True):
         """
         Sets account settings for balance warnings
         
         Args:
-            warn_threshold: The value at which account balance warnings are sent
+            warn_threshold: The value at which account balance warnings are
+                            sent
             warn_threshold_enabled (bool): Enable low balance warnings 
             warn_empty_enabled (bool): Enable empty balance warnings
 
@@ -178,22 +178,29 @@ class WhoisXMLAPI(object):
 
         return self._request(endpoint, params)
 
-    def whois(self, domain_name, da=0, ip=True, check_proxy_data=True, thin_whois=False, callback=None,
+    def whois(self, domain_name, prefer_fresh=False, da=0, ip=True,
+              check_proxy_data=True, thin_whois=False, callback=None,
               parse=False, registry_raw_text=None, registrar_raw_text=None):
         """
         Returns parsed WHOIS data
         
         Args:
             domain_name (str): A domain name or IP address
-            prefer_fresh (bool): Get the latest WHOIS record even if it's incomplete
-            da (int): 0 - Do not check for domain amiability; 1 - quick check; 2  - slower, but most accurate
+            prefer_fresh (bool): Get the latest WHOIS record even if it's
+                                incomplete
+            da (int): 0 - Do not check for domain amiability; 1 - quick check;
+                      2  - slower, but most accurate
             ip (bool): Resolve IP addresses
-            check_proxy_data (bool):  Check if registration matches known proxies
-            thin_whois (bool):  Only return data on the registrant, not the registrar                                                                                                                                                
+            check_proxy_data (bool):  Check if registration matches known
+                                      proxies
+            thin_whois (bool):  Only return data on the registrant, not the
+                                registrar
             callback (str): JSONP callback 
             parse (bool): Parse the provided raw WHOIS data
-            registry_raw_text (str): Optional raw WHOIS registration data to parse, rather than fetching it via the API 
-            registrar_raw_text (str: Optional raw WHOIS registration data to parse, rather than fetching it via the API
+            registry_raw_text (str): Optional raw WHOIS registration data to
+                                     parse, rather than fetching it via the API
+            registrar_raw_text (str): Optional raw WHOIS registration data to
+                                     parse, rather than fetching it via the API
 
         Returns:
             dict: Parsed WHOIS data
@@ -202,6 +209,8 @@ class WhoisXMLAPI(object):
         endpoint = "/whoisserver/WhoisService"
 
         params = dict(domainName=domain_name, da=da)
+        if prefer_fresh:
+            params["preferFresh"] = 1
         if da:
             params["da"] = 1
         if ip:
@@ -233,7 +242,9 @@ class WhoisXMLAPI(object):
             domains (list): A list of domains to get WHOIS records for
 
         Returns:
-            dict: keys: ``structured`` - parsed data in a dictionary, ``csv``: results in CSV format
+            dict: A dictionary with  keys:
+                - ``structured`` - parsed data in a dictionary
+                - ``csv``: results in CSV format
         """
         if type(domains) != list:
             raise ValueError("domains must be a list")
@@ -257,9 +268,11 @@ class WhoisXMLAPI(object):
         structured = response["whoisRecords"]
 
         for result in structured:
-            result["domainFetchedTime"] = epoch_to_datetime(result["domainFetchedTime"])
+            result["domainFetchedTime"] = epoch_to_datetime(
+                result["domainFetchedTime"])
 
-        url = "https://www.whoisxmlapi.com/BulkWhoisLookup/bulkServices/download"
+        url = "https://www.whoisxmlapi.com/BulkWhoisLookup/bulkServices" \
+              "/download"
         csv = ""
         time.sleep(15)
         response = self._session.post(url, json=params).text
@@ -270,7 +283,8 @@ class WhoisXMLAPI(object):
 
         return dict(structured=structured, csv=csv)
 
-    def reverse_whois(self, terms, exclude_terms=None, search_type="current", mode="preview",
+    def reverse_whois(self, terms, exclude_terms=None,
+                      search_type="current", mode="preview",
                       created_date_to=None, created_date_from=None,
                       updated_date_to=None, updated_date_from=None,
                       expired_date_to=None, expired_date_from=None):
@@ -282,12 +296,18 @@ class WhoisXMLAPI(object):
             exclude_terms (list): Terms to filter by 
             search_type (str): current or historic 
             mode (str): preview or purchase
-            created_date_to (str): Search through domains created before the given date (``YYYY-MM-DD`` format)
-            created_date_from (str): Search through domains created after the given date (``YYYY-MM-DD`` format)
-            updated_date_to (str): Search through domains created before the given date (``YYYY-MM-DD`` format)
-            updated_date_from (str): Search through domains created after the given date (``YYYY-MM-DD`` format)
-            expired_date_to (str): Search through domains created before the given date (``YYYY-MM-DD`` format)
-            expired_date_from (str): Search through domains created after the given date (``YYYY-MM-DD`` format)
+            created_date_to (str): Search through domains created before the
+            given date (``YYYY-MM-DD`` format)
+            created_date_from (str): Search through domains created after the
+            given date (``YYYY-MM-DD`` format)
+            updated_date_to (str): Search through domains created before the
+            given date (``YYYY-MM-DD`` format)
+            updated_date_from (str): Search through domains created after the
+            given date (``YYYY-MM-DD`` format)
+            expired_date_to (str): Search through domains created before the
+            given date (``YYYY-MM-DD`` format)
+            expired_date_from (str): Search through domains created after the
+            given date (``YYYY-MM-DD`` format)
 
         Returns:
             dict: A dictionary of preview data
@@ -315,10 +335,11 @@ class WhoisXMLAPI(object):
         if mode.lower() not in ["preview", "purchase"]:
             raise ValueError("mode must be preview or purchase")
 
-        params["basicSearchTerms"] = dict(include=terms, exclude=exclude_terms)
+        params["basicSearchTerms"] = dict(include=terms,
+                                          exclude=exclude_terms)
 
         balance = None
-        #balance = self.get_account_balances()["reverse_whois_balance"]
+        # balance = self.get_account_balances()["reverse_whois_balance"]
         if balance is None:
             balance = 0
 
@@ -335,18 +356,21 @@ class WhoisXMLAPI(object):
 
         return results
 
-    def brand_alert(self, terms, exclude_terms=None, since_date=None, days_back=None):
+    def brand_alert(self, terms, exclude_terms=None, since_date=None,
+                    days_back=None):
         """
         Lists newly created or deleted domains based on brand terms
         
         Args:
             terms (list): Brand terms to include in the search
             exclude_terms (list): Terms to exclude 
-            since_date (str): Only return domains created or deleted since this date, in YYYY-MM-DD format 
+            since_date (str): Only return domains created or deleted since this
+            date, in YYYY-MM-DD format
             days_back (int): The number of days back to search (12 maximum) 
 
         Returns:
-            list: A list of dictionaries containing the domain, and its status (i.e. new or deleted)
+            list: A list of dictionaries containing the domain, and its
+            status (i.e. new or deleted)
         """
         endpoint = "/brand-alert-api/search.php"
         params = dict(since_date=since_date, days_back=days_back)
@@ -355,7 +379,8 @@ class WhoisXMLAPI(object):
         if len(terms) > 4:
             raise ValueError("Number of terms cannot be greater than 4")
         if len(exclude_terms) > 4:
-            raise ValueError("Number of excluded terms cannot be greater than 4")
+            raise ValueError(
+                "Number of excluded terms cannot be greater than 4")
         if days_back and days_back > 12:
             raise ValueError("days_back cannot be greater than 12")
         for i in range(len(terms)):
@@ -367,18 +392,21 @@ class WhoisXMLAPI(object):
 
         return self._request(endpoint, params)["alerts"]
 
-    def registrant_alert(self, terms, exclude_terms=None, since_date=None, days_back=None):
+    def registrant_alert(self, terms, exclude_terms=None, since_date=None,
+                         days_back=None):
         """
         Lists newly created or deleted domains based on registrant terms
 
         Args:
             terms (list): Registrant terms to include in the search
             exclude_terms (list): Terms to exclude 
-            since_date (str): Only return domains created or deleted since this date, in YYYY-MM-DD format 
+            since_date (str): Only return domains created or deleted since
+            this date, in YYYY-MM-DD format
             days_back (int): The number of days back to search (12 maximum) 
 
         Returns:
-            list: A list of dictionaries containing the domain, and its status (i.e. new or deleted)
+            list: A list of dictionaries containing the domain, and its
+            status (i.e. new or deleted)
         """
         endpoint = "/registrant-alert-api/search.php"
         params = dict(since_date=since_date, days_back=days_back)
@@ -387,7 +415,8 @@ class WhoisXMLAPI(object):
         if len(terms) > 4:
             raise ValueError("Number of terms cannot be greater than 4")
         if len(exclude_terms) > 4:
-            raise ValueError("Number of excluded terms cannot be greater than 4")
+            raise ValueError(
+                "Number of excluded terms cannot be greater than 4")
         if days_back and days_back > 12:
             raise ValueError("days_back cannot be greater than 12")
         for i in range(len(terms)):
@@ -404,16 +433,19 @@ class WhoisXMLAPI(object):
         Preforms a DNS lookup
         Args:
             domain_name (str): The domain name or sub-domain to lookup
-            record_type (str): The DNS resource record type to query for, or ``_all`` for all record types
+            record_type (str): The DNS resource record type to query for, or
+            ``_all`` for all record types
             callback (str): A JSONP callback 
 
         Returns:
             dict: Lookup results
-            str: lookup results data wrapped in JSONP if ``callback`` was specified
+            str: lookup results data wrapped in JSONP if ``callback`` was
+            specified
 
         """
         endpoint = "/whoisserver/DNSService"
-        params = dict(domainName=domain_name, recordType=record_type, callback=callback)
+        params = dict(domainName=domain_name, recordType=record_type,
+                      callback=callback)
 
         return self._request(endpoint, params)
 
@@ -429,7 +461,8 @@ class WhoisXMLAPI(object):
             bool: The availability of the domain name
         """
         endpoint = "/whoisserver/WhoisService"
-        params = dict(cmd="GET_DN_AVAILABILITY", getMode=mode.upper(), domainName=domain_name)
+        params = dict(cmd="GET_DN_AVAILABILITY", getMode=mode.upper(),
+                      domainName=domain_name)
 
         if mode.upper() not in ["DNS_ONLY", "DNS_AND_WHOIS"]:
             raise ValueError("Mode must be DNS_ONLY or DNS_AND_WHOIS")
@@ -449,7 +482,8 @@ class WhoisXMLAPI(object):
         """
 
         url = "https://emailverification.whoisxmlapi.com/api/v1"
-        params = dict(apiKey=self._api_key, emailAddress=email_address, format="JSON")
+        params = dict(apiKey=self._api_key, emailAddress=email_address,
+                      format="JSON")
         response = self._session.get(url, params=params)
         response.raise_for_status()
 
