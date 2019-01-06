e8fc8c080abbb71befd438f13565152e56cf1b61
Sean Whalen <whalenster@gmail.com>
2019-01-06 13:49:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip install -U -r requirements.txt && rstcheck --report warning README.rst && cd docs && make html && cp -r _build/html/* ../../pywhoisxmlapi-docs/ && cd .. && flake8 pywhoisxmlapi.py && rm -rf dist/ build/ && python3 setup.py sdist && python3 setup.py bdist_wheel
+pip install -U -r requirements.txt && rstcheck --report warning README.rst && cd docs && make html && cp -r _build/html/* ../../pywhoisxmlapi-docs/ && cd .. && flake8 pywhoisxmlapi.py scripts/whois.py && rm -rf dist/ build/ && python3 setup.py sdist && python3 setup.py bdist_wheel
